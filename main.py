74d52f607fb1d85370c37dc170ded501581c7a4f
arooj-l <aliaqat@udel.edu>
2026-02-23 06:49:03 -0500

    structure for main

---
+++ main.py
@@ -0,0 +1,21 @@
+#garde_calculator 
+
+#list to hold all the grades
+marks = []
+
+#loop to add marks into the list until user enter done
+while True:
+    user_input = input("Enter mark or type done: ")
+
+    if user_input == "done":
+        break
+
+    marks.append(user_input)
+#test to see if list contains all teh element
+#print(marks)
+
+total = sum(marks)
+
+print(total)
+
+
