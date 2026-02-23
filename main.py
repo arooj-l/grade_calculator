d4fe9c51e3a8be2ba3788d0d4e8aa3ff8d8fa13f
arooj-l <aliaqat@udel.edu>
2026-02-23 06:57:24 -0500

    Addded error handling

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ marks = []
 #loop to add marks into the list until user enter done
 while True:
     user_input = input("Enter mark or type done: ")
-
-    if user_input == "done":
+#Error Handling, if the user types any string (abc ...)
+    if user_input.lower() == "done": #if user enter upper case
         break
+    
+    try:
+        mark = float(user_input)
+        marks.append(mark)
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
-    marks.append(float(user_input))
+   
 #test to see if list contains all teh element
 #print(marks)
 
